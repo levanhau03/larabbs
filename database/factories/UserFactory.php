82dbf2e6b00199eb3b717262f020d09f63ec8eec
levanhau03 <nhatki888@gmail.com>
2020-08-19 01:28:52 +0700

    vh

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,7 +10,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'remember_token' => Str::random(10),
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$Jw6BWylzajCk2erjJVBynukXLj/4aFFmOHBMvDj1.XC6qgBqZ0ax6', // 123456
         'introduction' => $faker->sentence(),
         'created_at' => $date_time,
         'updated_at' => $date_time,
