bc62e5371a6f495c4069eaad7d87288a6c782be0
levanhau03 <nhatki888@gmail.com>
2020-08-19 10:09:15 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'remember_token' => Str::random(10),
+        'remember_token' => Str::random(5),
         'email_verified_at' => now(),
         'password' => '$2y$10$Jw6BWylzajCk2erjJVBynukXLj/4aFFmOHBMvDj1.XC6qgBqZ0ax6', // 123456
         'introduction' => $faker->sentence(),
