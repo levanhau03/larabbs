d7529dffe76736bba61de2cc1f738438fe7b615c
levanhau03 <nhatki888@gmail.com>
2020-08-19 15:54:04 +0700

    111

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -22,7 +22,7 @@ class RepliesController extends Controller
         $reply->topic_id = $request->topic_id;
         $reply->save();
 
-        return redirect()->to($reply->topic->link())->with('success', '评论创建成功！');
+        return redirect()->to($reply->topic->link())->with('success', 'Đã tạo bình luận thành công!');
     }
 
     public function destroy(Reply $reply)
@@ -30,6 +30,6 @@ class RepliesController extends Controller
         $this->authorize('destroy', $reply);
         $reply->delete();
 
-        return redirect()->to($reply->topic->link())->with('success', '评论删除成功！');
+        return redirect()->to($reply->topic->link())->with('success', 'Đã xóa bình luận thành công!');
     }
 }
